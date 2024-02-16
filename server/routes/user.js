ba0a53486d2d1d60e60d4b30e2cf831bf4114f71
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2024-02-16 12:21:37 +0530

    updateUser added

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,7 +8,7 @@ router.get('/:email', userCtrl.getUserByEmail);
 //POST
 router.post('/newuser', userCtrl.createUser);
 //UPDATE
-
+router.update('/update', userCtrl.updateUser)
 //DELETE
 
 module.exports = router;
